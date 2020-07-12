6e814aedb129f6b89009e888b9181d67f86b8eed
Justin Brathwaite <jbrath110@yahoo.com>
2020-07-12 17:45:52 -0400

    New sort algos

---
+++ Tests/Sorting/SortingTests.java
@@ -0,0 +1,21 @@
+package Sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+import Algorithms.Sorting.*;
+
+public class SortingTests {
+    @Test
+    public void TestBubbleSort() {
+        var array = new int [] { 4,3,2,1};
+        var sortedArray = MyBubbleSort.Sort(array);
+        Assert.assertArrayEquals(new int []{1,2,3,4}, sortedArray );
+    }
+
+    @Test
+    public void TestQuickSort() {
+        var array = new int [] { 10, 80,30,90,40,50,70};
+        var sortedArray = QuickSort.Sort(array,0,array.length-1);
+        Assert.assertArrayEquals(new int []{10, 30,40,50,70,80,90}, sortedArray );
+    }
+}
---
+++ src/Algorithms.Sorting/QuickSort.java
@@ -0,0 +1,40 @@
+package Algorithms.Sorting;
+
+public class QuickSort {
+    public static int[] Sort(int [] array, int low, int high)
+    {
+        if(low < high) {
+            int pi = Partition(array, low, high);
+            Sort(array, low, pi - 1);
+            Sort(array, pi + 1, high);
+        }
+        return array;
+    }
+
+    public static int Partition(int [] array, int low, int high){
+        int i = low - 1;
+        int pivot = array[high]; // Set Pivot as the last value in array
+
+        /* Loop through the array and check if the current value is less than the pivot.
+           Compare the current loop value to a pointer i. Then swap i and j. If the current
+           value is greater than the pivot then increment j. i + 1 will end up being where the
+           pivot should be located in the array.
+         */
+        for(int j = low; j < high; j++ ){
+            if(array[j] < pivot){
+                i++;
+                int temp = array[i];
+                array[i]  = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // One last swap
+        int temp = array[i + 1];
+        array[i + 1]  = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+
+}
